198b7db2eb1039dd7b915a9a759ad94f1315e2f6
Prasad Patra <114677481+Seventy7dot2@users.noreply.github.com>
2023-12-05 09:31:05 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 export default function Navbar() {
-  return <img src="/image 1.png" alt="" />
+  return <>
+    <img src="/image 1.png" alt="" />
     <p>hello</p>;
+  </>
 }
