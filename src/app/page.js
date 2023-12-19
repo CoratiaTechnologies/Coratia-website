26ca15fb0ad5775082ef607092d4c41bead30f81
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2023-12-20 03:15:02 +0530

    Added preloader

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,37 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import "./globals.css";
 import Hero from "../components/hero/hero";
 import AboutUs from "../components/About/About";
 import Map from "../components/map/map";
+import HashLoader from "react-spinners/HashLoader";
+
+async function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 export default function Home() {
-  return(
+  let [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 700);
+  });
+  return (
     <>
-    <Hero />
-    <AboutUs />
-    <Map />
+      {loading ? (
+        <main className="flex w-[100vw] h-[100vh] items-center justify-center">
+          <HashLoader color="#0093C9" loading={loading} size={150} />
+        </main>
+      ) : (
+        <>
+          <Hero />
+          <AboutUs />
+          <Map />
+        </>
+      )}
     </>
-  )
+  );
 }
