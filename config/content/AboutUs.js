206426d9b239d4716209d52ce72bb600badc3898
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2023-12-20 16:04:58 +0530

    Added config mapping

---
+++ config/content/AboutUs.js
@@ -0,0 +1,6 @@
+export const aboutcontent = {
+  video: "https://www.youtube.com/embed/e1lyC_ipKfQ?si=eKFh_vWLLhENDdQs",
+  content: `We, a team of alumni and faculties from NIT Rourkela, formed a company named Coratia Technologies Pvt. Ltd.
+    with an aim to provide end to end solution in the field of AUV and ROV by reducing the costs linked to
+    underwater operations and inspection. At present we are incubated at IHFC, IIT Delhi and FTBI, NIT Rourkela.`,
+};
--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,14 @@ import "./globals.css";
 import Hero from "../components/hero/hero";
 import AboutUs from "../components/About/About";
 import Map from "../components/map/map";
-
+import Testimonials from "../components/Testimonials/Testimonials";
 export default function Home() {
-  return(
+  return (
     <>
-    <Hero />
-    <AboutUs />
-    <Map />
+      <Hero />
+      <AboutUs />
+      <Testimonials />
+      <Map />
     </>
-  )
+  );
 }
