2169f8e737daca391d9f1d85957d807008246947
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2023-12-27 17:01:00 +0530

    Added testimonial

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,6 +1,6 @@
 "use client";
-import React, { useState } from 'react';
-import Slider from 'react-slick';
+import React, { useState } from "react";
+import Slider from "react-slick";
 import "./Testimonial.css";
 import styled from "styled-components";
 
@@ -8,20 +8,26 @@ const carouselContent = [
   {
     id: 1,
     picture: "https://i.ibb.co/ncrXc2V/1.png",
-    text: 'Testimonial text 1',
-    speaker: 'Speaker 1',
+    text: `Coratia Technologies' vision in the area of Robotics,
+    especially AUVs will herald a new beginning in Underwater
+    Surveillance of the water reservoirs in Steel Industries.`,
+    speaker: "Speaker 1",
   },
   {
     id: 2,
     picture: "https://i.ibb.co/B3s7v4h/2.png",
-    text: 'Testimonial text 2',
-    speaker: 'Speaker 2',
+    text: `Coratia Technologies' vision in the area of Robotics,
+    especially AUVs will herald a new beginning in Underwater
+    Surveillance of the water reservoirs in Steel Industries.`,
+    speaker: "Speaker 2",
   },
   {
     id: 3,
     picture: "https://i.ibb.co/XXR8kzF/3.png",
-    text: 'Testimonial text 3',
-    speaker: 'Speaker 3',
+    text: `Coratia Technologies' vision in the area of Robotics,
+    especially AUVs will herald a new beginning in Underwater
+    Surveillance of the water reservoirs in Steel Industries.`,
+    speaker: "Speaker 3",
   },
 ];
 
@@ -40,16 +46,36 @@ const Heading = styled.div`
   }
 `;
 
+function QuoteIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M3 21c3 0 7-1 7-8V5c0-1.25-.756-2.017-2-2H4c-1.25 0-2 .75-2 1.972V11c0 1.25.75 2 2 2 1 0 1 0 1 1v1c0 1-1 2-2 2s-1 .008-1 1.031V20c0 1 0 1 1 1z" />
+      <path d="M15 21c3 0 7-1 7-8V5c0-1.25-.757-2.017-2-2h-4c-1.25 0-2 .75-2 1.972V11c0 1.25.75 2 2 2h.75c0 2.25.25 4-2.75 4v3c0 1 0 1 1 1z" />
+    </svg>
+  );
+}
+
 function CustomPrevArrow({ onClick }) {
   return (
     <div
       className="arrow2 custom-prev-arrow2"
       onClick={onClick}
       onKeyDown={onClick}
-      role='button'
+      role="button"
       tabIndex={0}
     >
-      <span className='material-symbols-outlined'>&#8249;</span>
+      <span className="material-symbols-outlined">&#8249;</span>
     </div>
   );
 }
@@ -60,10 +86,10 @@ function CustomNextArrow({ onClick }) {
       className="arrow2 custom-next-arrow2"
       onClick={onClick}
       onKeyDown={onClick}
-      role='button'
+      role="button"
       tabIndex={0}
     >
-      <span className='material-symbols-outlined'>&#8250;</span>
+      <span className="material-symbols-outlined">&#8250;</span>
     </div>
   );
 }
@@ -75,7 +101,7 @@ export default function Testimonials() {
     infinite: true,
     speed: 1200,
     centerMode: true,
-    centerPadding: '0px',
+    centerPadding: "0px",
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
@@ -95,43 +121,71 @@ export default function Testimonials() {
     ],
   };
 
-
   return (
     <>
-    <div className="bg-white flex items-center justify-center">
-      <Heading>TESTIMONIALS</Heading>
-    </div>
+      <div className="bg-white flex items-center justify-center">
+        <Heading className="mt-10 mb-10">TESTIMONIALS</Heading>
+      </div>
 
-    <div className="carouselContainer2">
-      <div className="slider2">
-        <Slider
-          centerMode={settings.centerMode}
-          centerPadding={settings.centerPadding}
-          slidesToShow={settings.slidesToShow}
-          slidesToScroll={settings.slidesToScroll}
-          autoplay={settings.autoplay}
-          autoplaySpeed={settings.autoplaySpeed}
-          beforeChange={settings.beforeChange}
-          prevArrow={settings.prevArrow}
-          nextArrow={settings.nextArrow}
-          responsive={settings.responsive}
-        >
-          {carouselContent.map((content, index) => (
-            <div className={index === slideIndex ? 'slide2 slide-active2' : 'slide2'} key={content}>
-
-              <div className='wrapper2 items-center justify-center px-5 md:px-20'>
-                <img className='carousel-img rounded-full object-cover' src={content.picture} alt={`Alt ${index}`} />
-                <div className='text-black text-xl text-center mt-5 md:mt-0 md:mx-10'>
-                  <p>{content.text}</p>
-                  <p className='text-sm'>-{content.speaker}</p>
+      <div className="carouselContainer2">
+        <div className="slider2">
+          <Slider
+            centerMode={settings.centerMode}
+            centerPadding={settings.centerPadding}
+            slidesToShow={settings.slidesToShow}
+            slidesToScroll={settings.slidesToScroll}
+            autoplay={settings.autoplay}
+            autoplaySpeed={settings.autoplaySpeed}
+            beforeChange={settings.beforeChange}
+            prevArrow={settings.prevArrow}
+            nextArrow={settings.nextArrow}
+            responsive={settings.responsive}
+          >
+            {carouselContent.map((content, index) => (
+              <div
+                className={`${
+                  index === slideIndex ? "slide2 slide-active2" : "slide2"
+                } flex items-center justify-between`}
+                key={content}
+              >
+                <div className="flex flex-col md:flex-row space-y-6 md:space-y-0 space-x-0 md:space-x-6">
+                  <div className="flex flex-col items-center">
+                    <img
+                      alt="Mrs. Atashi Pramanik"
+                      className="rounded-lg border-4"
+                      height="200"
+                      src={content.picture}
+                      style={{
+                        aspectRatio: "200/200",
+                        objectFit: "cover",
+                      }}
+                      width="200"
+                    />
+                    <p className="mt-4 text-lg font-semibold">
+                      Mrs. Atashi Pramanik
+                    </p>
+                    <p className="text-sm text-gray-600">
+                      Ex - CGM In-Charge (Automation, Instrumentation & IT)
+                    </p>
+                    <p className="text-sm text-gray-600">
+                      Rourkela Steel Plant, SAIL
+                    </p>
+                  </div>
+                  <div className="relative max-w-lg">
+                    <QuoteIcon className="absolute top-0 left-0 text-gray-400 w-12 h-12 transform rotate-180" />
+                    <p className="text-lg italic px-14 py-8 text-black">
+                      Coratia Technologies' vision in the area of Robotics,
+                      especially AUVs will herald a new beginning in Underwater
+                      Surveillance of the water reservoirs in Steel Industries.
+                    </p>
+                    <QuoteIcon className="absolute bottom-0 right-0 text-gray-400 w-12 h-12" />
+                  </div>
                 </div>
               </div>
-
-            </div>
-          ))}
-        </Slider>
+            ))}
+          </Slider>
+        </div>
       </div>
-    </div>
     </>
   );
 }
