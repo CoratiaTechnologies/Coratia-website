4e06f88cc3a6028553ec4107396d9bbbc74a0131
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2023-12-31 17:54:56 +0530

    Added config content of map n footer

---
+++ config/content/Map.js
@@ -0,0 +1,5 @@
+export const mapcontent = {
+  gmap_link: `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3692.612310425271!2d84.90100261503333!3d22.25478378534724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a201dd5fad8f227%3A0x933f1d1680089272!2sTechnology%20Innovation%20and%20Industry%20Relation%20Center%2C%20Northern%20Ave%2C%20National%20Institute%20of%20Technology%2C%20Sector%201%2C%20Rourkela%2C%20Odisha%20769008!5e0!3m2!1sen!2sin!4v1624822367801!5m2!1sen!2sin`,
+  address: `Coratia Technologies Pvt. Ltd. Foundation for Technology & Business
+  Incubation, NIT Rourkela, Odisha`,
+};
---
+++ config/content/Socials.js
@@ -0,0 +1,7 @@
+export const socials = {
+  linkedin: "https://www.linkedin.com/company/coratia-technologies-pvt-ltd/",
+  facebook: "https://www.facebook.com/coratiatech",
+  instagram: "https://www.instagram.com/coratiatech/",
+  twitter: "https://twitter.com/CoratiaTech",
+  youtube: "https://www.youtube.com/channel/UClKU1kWF0zjiNihlG-Lfrmw",
+};
