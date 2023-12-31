61c4e7cfa9b5eed83371b620626c3680c5c0a99b
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2023-12-31 17:55:53 +0530

    Added Typography

---
+++ src/components/Typography.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Heading = styled.div`
+  font-family: Poppins;
+  font-size: 5.5rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  background: linear-gradient(180deg, #749cac 0%, rgba(0, 0, 0, 0) 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  @media (max-width: 768px) {
+    font-size: 2.5rem;
+  }
+`;
