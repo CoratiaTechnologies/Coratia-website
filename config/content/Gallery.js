735d53e29aa6845b003fe9419e224e8f763ae36f
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2024-01-02 19:11:44 +0530

    Added config for gallery

---
+++ config/content/Gallery.js
@@ -0,0 +1,29 @@
+export const gallerycontent = [
+  {
+    title: "Heading 1",
+    images: [
+      "https://res.cloudinary.com/dhnkuonev/image/upload/v1702296582/pmtopml0tqukabudsrvp.jpg",
+      "https://res.cloudinary.com/dhnkuonev/image/upload/v1702296581/q3xgywx6x0pdqjymuyld.jpg",
+      "https://res.cloudinary.com/dhnkuonev/image/upload/v1702296579/ex0vix6wbrx7g5hlot2w.jpg",
+      "https://res.cloudinary.com/dhnkuonev/image/upload/v1702296987/uveuklce3owmbgbymozg.jpg",
+    ],
+  },
+  {
+    title: "Heading 2",
+    images: [
+      "https://res.cloudinary.com/dhnkuonev/image/upload/v1702296582/pmtopml0tqukabudsrvp.jpg",
+      "https://res.cloudinary.com/dhnkuonev/image/upload/v1702296581/q3xgywx6x0pdqjymuyld.jpg",
+      "https://res.cloudinary.com/dhnkuonev/image/upload/v1702296579/ex0vix6wbrx7g5hlot2w.jpg",
+      "https://res.cloudinary.com/dhnkuonev/image/upload/v1702296987/uveuklce3owmbgbymozg.jpg",
+    ],
+  },
+  {
+    title: "Heading 3",
+    images: [
+      "https://res.cloudinary.com/dhnkuonev/image/upload/v1702296582/pmtopml0tqukabudsrvp.jpg",
+      "https://res.cloudinary.com/dhnkuonev/image/upload/v1702296581/q3xgywx6x0pdqjymuyld.jpg",
+      "https://res.cloudinary.com/dhnkuonev/image/upload/v1702296579/ex0vix6wbrx7g5hlot2w.jpg",
+      "https://res.cloudinary.com/dhnkuonev/image/upload/v1702296987/uveuklce3owmbgbymozg.jpg",
+    ],
+  },
+];
