09d473998ce52873ee9ec2e8db35a433329bf3a4
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2024-01-03 18:39:27 +0530

    achievements: cursor pointer

--- src/components/Achievements/Achievement.jsx
+++ src/components/Achievements/Achievement.jsx
@@ -46,9 +46,10 @@ const achievements = [
 export default function Achievements() {
   return (
     <>
-    <style>{`
+      <style>{`
       .card:hover .hover-text{
         display: flex;
+        cursor: pointer;
       }
     `}</style>
 
@@ -63,17 +64,20 @@ export default function Achievements() {
               className="card border-2 mx-auto shadow-xl md:w-[540px] md:h-[340px] w-[300px] h-[240px] overflow-hidden"
             >
               <div>
-              <div 
-                className="text-center text-sm md:text-base text-black items-center
-                hidden hover-text relative h-[240px] md:h-[340px] z-30 w-full">
-                {item.desc}
+                <div
+                  className="text-center text-sm md:text-base text-black items-center
+                hidden hover-text relative h-[240px] md:h-[340px] z-30 w-full"
+                >
+                  {item.desc}
+                </div>
+                <img
+                  src={item.picture}
+                  className="md:h-[280px] h-[170px] w-full rounded-sm object-cover"
+                />
               </div>
-              <img
-                src={item.picture}
-                className="md:h-[280px] h-[170px] w-full rounded-sm object-cover"
-              />
-              </div>
-              <p className="text-center text-sm md:text-base text-black">{item.text}</p>
+              <p className="text-center text-sm md:text-base text-black">
+                {item.text}
+              </p>
             </div>
           ))}
         </div>
