c982de7fcc34886e9f7ab5e80526f901a8fa2077
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2024-01-12 02:13:17 +0530

    exp:lite yt

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { aboutcontent } from "../../../config/content/AboutUs";
 import { Heading } from "../Typography";
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
+
 export default function AboutUs() {
   return (
     <div className="text-black md:py-24 bg-white">
       <Heading className="text-center block md:hidden py-7">About Us</Heading>
       <div className="flex flex-wrap w-full">
         <div className="w-full md:w-1/2 px-4 md:px-10">
-          <iframe
+          {/* <iframe
             alt="Team working"
             className="object-cover w-full h-full rounded-md"
             height="500"
@@ -18,6 +21,18 @@ export default function AboutUs() {
               objectFit: "cover",
             }}
             width="913"
+          /> */}
+          <LiteYouTubeEmbed
+            id="e1lyC_ipKfQ"
+            title="Why do you need us ?"
+            poster="maxresdefault"
+            webp
+            style={{
+              aspectRatio: "900/500",
+              objectFit: "cover",
+              height: "500",
+              borderRadius: "5px",
+            }}
           />
         </div>
         <div className="w-full md:w-1/2 p-6 md:p-12">
