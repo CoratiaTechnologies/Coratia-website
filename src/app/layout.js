a92bd645322ddd66af58a46211084e8575fc8cf7
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2024-01-12 00:36:53 +0530

    Added meta data and keywords

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,43 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Coratia Technologies Ltd.",
+  description:
+    "Coratia Technologies Pvt. Ltd. is a trailblazing company specializing in underwater robotics. We offer comprehensive solutions in the field of Remotely Operated Vehicles (ROVs) and Autonomous Underwater Vehicles (AUVs), catering to a wide range of underwater challenges using cutting-edge technologies.",
+  keywords: [
+    "coratia",
+    "underwater",
+    "robotics",
+    "rov",
+    "auv",
+    "subsea",
+    "marine",
+    "exploration",
+    "ocean",
+    "engineering",
+    "inspection",
+    "offshore",
+    "drones",
+    "aquatic",
+    "innovations",
+    "deepsea",
+    "automation",
+    "imaging",
+    "intervention",
+    "hydrodynamic",
+    "sensing",
+    "design",
+    "integration",
+    "services",
+    "technology",
+    "solutions",
+    "hydrographic",
+    "surveys",
+    "vehicles",
+    "sensors",
+    "oceanographic",
+    "India",
+    "NIT Rourkela",
+  ],
 };
 
 export default function RootLayout({ children }) {
