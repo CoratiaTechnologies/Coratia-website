01aa5d4478070f1d8a3672103a0c9d8ff5de0945
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2024-01-16 00:36:46 +0530

    added social cards

--- src/app/layout.js
+++ src/app/layout.js
@@ -44,6 +44,32 @@ export const metadata = {
     "India",
     "NIT Rourkela",
   ],
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: "https://coratia.netlify.app/",
+    title: "Coratia Technologies Ltd.",
+    description:
+      "Coratia Technologies Pvt. Ltd. is a trailblazing company specializing in underwater robotics. We offer comprehensive solutions in the field of Remotely Operated Vehicles (ROVs) and Autonomous Underwater Vehicles (AUVs), catering to a wide range of underwater challenges using cutting-edge technologies.",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dgjzygzgx/image/upload/v1705345499/card_yjtbjk.png",
+        width: 800,
+        height: 600,
+      },
+    ],
+    site_name: "Vriddhi 2023",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Coratia Technologies Ltd.",
+    description:
+      "Coratia Technologies Pvt. Ltd. is a trailblazing company specializing in underwater robotics. We offer comprehensive solutions in the field of Remotely Operated Vehicles (ROVs) and Autonomous Underwater Vehicles (AUVs), catering to a wide range of underwater challenges using cutting-edge technologies.",
+
+    images: [
+      "https://res.cloudinary.com/dgjzygzgx/image/upload/v1705345499/card_yjtbjk.png",
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
