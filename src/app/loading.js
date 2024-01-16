0fb5ea843a8eac2ee2981dde0f31d4e048e9acbd
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2024-01-17 02:24:20 +0530

    exp:loader

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,9 +1,47 @@
 import React from "react";
-import "./skeleton.css";
 
 function loading() {
   return (
-    <div style={{ height: "100vh", width: "100vw" }} className="skeleton"></div>
+    <div className="flex w-[100vw] h-[100vh] items-center justify-center">
+      <div
+        style={{
+          display: "inherit",
+          position: "relative",
+          width: "70px",
+          height: "70px",
+          transform: "rotate(165deg)",
+        }}
+      >
+        <span
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            display: "block",
+            width: "14px",
+            height: "14px",
+            borderRadius: "7px",
+            transform: "translate(-50%, -50%)",
+            animation:
+              "2s ease 0s infinite normal none running react-spinners-HashLoader-before",
+          }}
+        ></span>
+        <span
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            display: "block",
+            width: "14px",
+            height: "14px",
+            borderRadius: "7px",
+            transform: "translate(-50%, -50%)",
+            animation:
+              "2s ease 0s infinite normal none running react-spinners-HashLoader-after",
+          }}
+        ></span>
+      </div>
+    </div>
   );
 }
 
--- src/app/services/loading.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-function loading() {
-  return (
-    <div style={{ height: "100%", width: "100%" }} className="skeleton"></div>
-  );
-}
-
-export default loading;
