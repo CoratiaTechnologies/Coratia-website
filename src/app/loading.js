7b876907a105faeeeb09956307a9ae2df3c2b5fe
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2024-01-17 00:27:38 +0530

    exp:loader

---
+++ src/app/loading.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./skeleton.css";
+
+function loading() {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }} className="skeleton"></div>
+  );
+}
+
+export default loading;
