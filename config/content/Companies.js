93b8b58d2cc39a2c1150927e9218b56b5076a344
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2024-01-20 00:51:52 +0530

    minor

--- config/content/Companies.js
+++ config/content/Companies.js
@@ -2,9 +2,9 @@ export const logos = [
   "https://res.cloudinary.com/dq9gdlbls/image/upload/f_auto,q_auto/jvicdgqwd4dnm6vfql8m",
   "https://res.cloudinary.com/dgjzygzgx/image/upload/v1705344301/indian_oil_diazr5.webp",
   "https://res.cloudinary.com/dq9gdlbls/image/upload/f_auto,q_auto/t0wa1rxfuapm0wv53ird",
-  "https://res.cloudinary.com/dq9gdlbls/image/upload/f_auto,q_auto/ibkzmwyfylnbsqjjrnwr",
-  "https://res.cloudinary.com/dq9gdlbls/image/upload/f_auto,q_auto/ut3yd1x6wspjdqszjwyn",
+  "https://res.cloudinary.com/dgjzygzgx/image/upload/v1705691398/Indian_Navy-removebg-preview_t6r2mv.png",
+  "https://res.cloudinary.com/dgjzygzgx/image/upload/v1705691398/Hindalco-removebg-preview_zyc9dj.png",
   "https://res.cloudinary.com/dq9gdlbls/image/upload/f_auto,q_auto/lcgsrqq3ljwjqxq1tu66",
-  "https://res.cloudinary.com/dq9gdlbls/image/upload/f_auto,q_auto/slaf9jk6jsgkca6ubxsg",
-  "https://res.cloudinary.com/dq9gdlbls/image/upload/f_auto,q_auto/zjp5eldvvzy8xdnsepzm",
+  "https://res.cloudinary.com/dgjzygzgx/image/upload/v1705691398/NINL-removebg-preview_sxojzn.png",
+  "https://res.cloudinary.com/dgjzygzgx/image/upload/v1705691398/Idex-removebg-preview_cpy5aq.png",
 ];
