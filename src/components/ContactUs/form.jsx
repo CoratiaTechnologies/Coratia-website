36800e8629e6b6eba50c7926ec876aea7cc4685c
Seventy7dot2 <22f3000994@ds.study.iitm.ac.in>
2024-03-30 06:45:52 +0530

    fix:styling

--- src/components/ContactUs/form.jsx
+++ src/components/ContactUs/form.jsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import { redirect } from "next/navigation";
 import "./form.css";
 
 export default function ContactUsForm() {
@@ -24,6 +25,8 @@ export default function ContactUsForm() {
       if (response.ok) {
         const responseData = await response.text();
         setMessage(responseData);
+        alert("Submitted! We'll contact you soon!");
+        window.location.href = "/";
       } else {
         setMessage("Error submitting form.");
       }
@@ -48,14 +51,15 @@ export default function ContactUsForm() {
                 htmlFor="first-name"
                 className="block text-white text-sm font-bold mb-2"
               >
-                First Name
+                First Name*
               </label>
               <input
                 id="first-name"
                 name="firstname"
                 type="text"
                 placeholder="First Name"
-                className="shadow appearance-none border rounded-lg w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded-lg w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
+                required
               />
             </div>
             <div className="w-full md:w-1/2 md:pl-2">
@@ -70,7 +74,7 @@ export default function ContactUsForm() {
                 name="lastname"
                 type="text"
                 placeholder="Last Name"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
               />
             </div>
           </div>
@@ -79,14 +83,15 @@ export default function ContactUsForm() {
               htmlFor="email"
               className="block text-white text-sm font-bold mb-2"
             >
-              Email
+              Email*
             </label>
             <input
               id="email"
               name="email"
               type="email"
               placeholder="Email"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
+              required
             />
           </div>
           <div className="mb-4">
@@ -94,14 +99,15 @@ export default function ContactUsForm() {
               htmlFor="phone"
               className="block text-white text-sm font-bold mb-2"
             >
-              Phone
+              Phone*
             </label>
             <input
               id="phone"
               name="phone"
               type="tel"
               placeholder="Phone"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+              required
             />
           </div>
           <div className="mb-4">
@@ -109,18 +115,19 @@ export default function ContactUsForm() {
               htmlFor="query"
               className="block text-white text-sm font-bold mb-2"
             >
-              Your Query
+              Your Query*
             </label>
             <textarea
               id="query"
               name="query"
               placeholder="Enter your query"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+              required
             ></textarea>
           </div>
           <div className="flex items-center justify-between">
             <input
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-[#41616d] mx-auto hover:bg-[#273e47] cursor-pointer text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="submit"
               value={submitting ? "Submitting..." : "Submit"}
             />
